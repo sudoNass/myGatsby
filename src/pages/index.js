eb467d1768457f1f45d09388de9196fcd2acf9a0
sudoNass <nass.asmari@gmail.com>
2023-05-29 21:15:45 +0300

    Update online resume3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,137 +1,108 @@
-import * as React from "react";
+import * as React from "react"
 
 const pageStyles = {
-  color: "#fff",
-  backgroundColor: "#1a1a1a",
+  backgroundColor: "#1e1e1e",
+  color: "#a0fba1",
+  padding: 96,
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
-  height: "100vh",
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  justifyContent: "center",
-  padding: "0 10%",
+}
+
+const backgroundStyles = {
+  position: "fixed",
+  top: 0,
+  left: 0,
+  height: "100%",
+  width: "100%",
+  zIndex: -1,
+  backgroundImage: `radial-gradient(#1e1e1e 1px, transparent 1px), radial-gradient(#1e1e1e 1px, transparent 1px)`,
+  backgroundSize: "3px 3px",
+  backgroundPosition: "0 0, 1.5px 1.5px",
 }
 
 const headingStyles = {
-  fontSize: "4rem",
-  margin: "0 0 2rem",
-  textAlign: "center",
-  textTransform: "uppercase",
-  letterSpacing: "0.3rem",
+  marginTop: 0,
+  marginBottom: 64,
+  maxWidth: 320,
 }
 
-const subheadingStyles = {
-  fontSize: "1.5rem",
-  margin: "0 0 4rem",
-  textAlign: "center",
-  fontWeight: "bold",
+const headingAccentStyles = {
+  color: "#66ff66",
 }
 
-const iconStyles = {
-  display: "inline-block",
-  width: "2rem",
-  height: "2rem",
-  marginRight: "1rem",
-  verticalAlign: "middle",
+const paragraphStyles = {
+  marginBottom: 48,
 }
 
-const sectionStyles = {
-  margin: "4rem 0",
-  padding: "2rem",
-  borderRadius: "0.5rem",
-  backgroundColor: "#333",
-  maxWidth: "50rem",
-  width: "100%",
-}
-
-const sectionHeadingStyles = {
-  fontSize: "2Here's the rest of the code:
-
-```jsx
-rem",
-  margin: "0 0 2rem",
-  textTransform: "uppercase",
-  letterSpacing: "0.2rem",
-  textAlign: "center",
-}
-
-const sectionSubheadingStyles = {
+const codeStyles = {
+  color: "#8A6534",
+  padding: 4,
+  backgroundColor: "#FFF4DB",
   fontSize: "1.25rem",
-  margin: "0 0 4rem",
-  textAlign: "center",
-  fontWeight: "bold",
+  borderRadius: 4,
+}
+
+const listStyles = {
+  marginBottom: 96,
+  paddingLeft: 0,
 }
 
 const listItemStyles = {
-  fontSize: "1.25rem",
-  margin: "0 0 2rem",
-  display: "flex",
-  alignItems: "center",
+  fontWeight: 300,
+  fontSize: 24,
+  maxWidth: 560,
+  marginBottom: 30,
 }
 
-const listIconStyles = {
-  ...iconStyles,
-  width: "1.5rem",
-  height: "1.5rem",
-  marginRight: "0.5rem",
-}
-
-const listTextStyles = {
+const linkStyle = {
+  color: "#66ff66",
   fontWeight: "bold",
+  fontSize: 16,
+  verticalAlign: "5%",
+}
+
+const descriptionStyle = {
+  color: "#a0fba1",
+  fontSize: 14,
+  marginTop: 10,
+  marginBottom: 0,
+  lineHeight: 1.25,
 }
 
 const IndexPage = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>John Doe</h1>
-      <h2 style={subheadingStyles}>Computer Engineer</h2>
-      <section style={sectionStyles}>
-        <h3 style={sectionHeadingStyles}>
-          <i className="bi bi-award" style={listIconStyles}></i>Education
-        </h3>
-        <p style={sectionSubheadingStyles}>Bachelor of Science in Computer Engineering</p>
-        <ul>
+    <>
+      <div style={backgroundStyles}></div>
+      <main style={pageStyles}>
+        <h1 style={headingStyles}>
+          John Doe
+          <br />
+          <span style={headingAccentStyles}>Computer Engineer</span>
+        </h1>
+        <p style={paragraphStyles}>
+          I am a computer engineer with a degree in XYZ from ABC University. I am experienced in various programming languages and frameworks, and I am passionate about creating efficient, scalable, and maintainable software solutions.
+        </p>
+        <h2>Projects</h2>
+        <ul style={listStyles}>
+          {/* Add your projects here */}
           <li style={listItemStyles}>
-            <i className="bi bi-check" style={listIconStyles}></i>
-            <span style={listTextStyles}>University of Engineering and Technology, Lahore</span>
+            <a style={linkStyle} href="https://github.com/username/project">
+              Project 1
+            </a>
+            <p style={descriptionStyle}>
+              A brief description of your first project.
+            </p>
+          </li>
+          <li style={listItemStyles}>
+            <a style={linkStyle} href="https://github.com/username/project">
+              Project 2
+            </a>
+            <p style={descriptionStyle}>
+              A brief description of your second project.
+            </p>
           </li>
         </ul>
-      </section>
-      <section style={sectionStyles}>
-        <h3 style={sectionHeadingStyles}>
-          <i className="bi bi-code-slash" style={listIconStyles}></i>Skills
-        </h3>
-        <p style={sectionSubheadingStyles}>Programming Languages</p>
-        <ul>
-          <li style={listItemStyles}>
-            <i className="bi bi-check" style={listIconStyles}></i>
-            <span style={listTextStyles}>C, C++, Java, Python</span>
-          </li>
-        </ul>
-        <p style={sectionSubheadingStyles}>Tools and Frameworks</p>
-        <ul>
-          <li style={listItemStyles}>
-            <i className="bi bi-check" style={listIconStyles}></i>
-            <span style={listTextStyles}>Arduino, Raspberry Pi, React, Node.js</span>
-          </li>
-        </ul>
-      </section>
-      <section style={sectionStyles}>
-        <h3 style={sectionHeadingStyles}>
-          <i className="bi bi-briefcase" style={listIconStyles}></i>Experience
-        </h3>
-        <ul>
-          <li style={listItemStyles}>
-            <i className="bi bi-briefcase" style={listIconStyles}></i>
-            <span style={listTextStyles}>Software Engineer, XYZ Company</span>
-          </li>
-          <li style={listItemStyles}>
-            <i className="bi bi-briefcase" style={listIconStyles}></i>
-            <span style={listTextStyles}>Intern, ABC Company</span>
-          </li>
-        </ul>
-      </section>
-    </main>
+      </main>
+    </>
   )
 }
 
