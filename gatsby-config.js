5769986aa7e008c1e6c3d5970a98d341d5a49ff8
sudoNass <nass.asmari@gmail.com>
2023-05-29 19:38:57 +0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Nasser's resume`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
