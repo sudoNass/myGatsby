a90827d088e24ad0e6658b4e10052a0daeea9ff8
sudoNass <nass.asmari@gmail.com>
2023-05-31 22:35:12 +0300

    try to solve

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ScrollSections from '../components/ScrollSections';
+
+function Home() {
+return (
+<div>
+<ScrollSections />
+</div>
+);
+}
+
+export default Home;
+
